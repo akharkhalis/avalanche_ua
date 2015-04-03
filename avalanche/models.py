0fa7f8ff70979abe28575171f028bdb81382343c
Anton Kharkhalis <anton.harhalis@gmail.com>
2015-04-03 20:23:36 +0300

    змінено:                    avalanche/admin.py 	змінено:                    avalanche/models.py 	змінено:                    db.sqlite3 	avalanche/migrations/0001_initial.py 	avalanche/static/ 	avalanche/templates/ 	avalanche/views/ 	avalanche_ua/context_processors.py 	avalanche_ua/map.html 	requirements.txt

--- avalanche/models.py
+++ avalanche/models.py
@@ -1,3 +1,87 @@
+# -*- coding: utf-8 -*-
+
 from django.db import models
 
-# Create your models here.
+class activity_db(models.Model):
+    """avalanch activity observation model"""
+
+    class Meta(object):
+        verbose_name = u"Лавина"
+        verbose_name_plural = u"Лавини"
+
+    date = models.DateField(
+        blank=False,
+        verbose_name=u"Дата",
+        null=True)
+
+    time = models.TimeField(
+        blank=False,
+        verbose_name=u"Час",
+        null=True)
+
+    coordinat_x = models.CharField(
+        max_length=256,
+        blank=True,
+        verbose_name=u"Довгота",
+        default='')
+
+    coordinat_y = models.CharField(
+        max_length=256,
+        blank=True,
+        verbose_name=u"Широта",
+        default='')
+
+    altitude = models.CharField(
+        max_length=256,
+        blank=True,
+        verbose_name=u"Висота",
+        default='')
+
+    victims = models.CharField(
+        max_length=256,
+        blank=True,
+        verbose_name=u"Потерпілі",
+        default='')
+
+    rel_volume = models.CharField(
+        max_length=256,
+        blank=True,
+        verbose_name=u"Відносний об`єм",
+        default='')
+
+
+   # zone = models.ForeignKey('Group',
+   #     verbose_name=u"Група",
+   #     blank=False,
+   #     null=True,
+   #     on_delete=models.PROTECT)
+
+    notes = models.TextField(
+        blank=True,
+        verbose_name=u"Додаткові нотатки")
+
+    photo = models.ImageField(
+        blank=True,
+        verbose_name=u"Фото",
+        null=True)
+
+#observer data
+    obs_first_name = models.CharField(
+        max_length=256,
+        blank=False,
+        verbose_name=u"Ім'я")
+    obs_last_name = models.CharField(
+        max_length=256,
+        blank=False,
+        verbose_name=u"Прізвище")
+    obs_phone = models.CharField(
+        max_length=256,
+        blank=False,
+        verbose_name=u"Телефон")
+    obs_mail = models.EmailField(
+        max_length=256,
+        blank=False,
+        verbose_name=u"Емейл")
+
+    def __unicode__(self):
+        return u"%s %s" % (self.date, self.obs_last_name)
